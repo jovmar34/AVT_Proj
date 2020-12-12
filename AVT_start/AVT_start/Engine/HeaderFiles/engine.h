fd67776656d5c7d193057ce5473a66dfe89f7db6
Joao Martinho <jovmar34@gmail.com>
2020-12-12 09:29:53 +0000

    Errors sent to engine.h

--- AVT_start/AVT_start/Engine/HeaderFiles/engine.h
+++ AVT_start/AVT_start/Engine/HeaderFiles/engine.h
@@ -14,3 +14,111 @@
 #include "camera.h"
 #include "sceneGraph.h"
 #include "shader.h"
+
+static const std::string errorSource(GLenum source)
+{
+	switch (source) {
+	case GL_DEBUG_SOURCE_API:				return "API";
+	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:		return "window system";
+	case GL_DEBUG_SOURCE_SHADER_COMPILER:	return "shader compiler";
+	case GL_DEBUG_SOURCE_THIRD_PARTY:		return "third party";
+	case GL_DEBUG_SOURCE_APPLICATION:		return "application";
+	case GL_DEBUG_SOURCE_OTHER:				return "other";
+	default:								exit(EXIT_FAILURE);
+	}
+}
+
+static const std::string errorType(GLenum type)
+{
+	switch (type) {
+	case GL_DEBUG_TYPE_ERROR:				return "error";
+	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:	return "deprecated behavior";
+	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:	return "undefined behavior";
+	case GL_DEBUG_TYPE_PORTABILITY:			return "portability issue";
+	case GL_DEBUG_TYPE_PERFORMANCE:			return "performance issue";
+	case GL_DEBUG_TYPE_MARKER:				return "stream annotation";
+	case GL_DEBUG_TYPE_PUSH_GROUP:			return "push group";
+	case GL_DEBUG_TYPE_POP_GROUP:			return "pop group";
+	case GL_DEBUG_TYPE_OTHER_ARB:			return "other";
+	default:								exit(EXIT_FAILURE);
+	}
+}
+
+static const std::string errorSeverity(GLenum severity)
+{
+	switch (severity) {
+	case GL_DEBUG_SEVERITY_HIGH:			return "high";
+	case GL_DEBUG_SEVERITY_MEDIUM:			return "medium";
+	case GL_DEBUG_SEVERITY_LOW:				return "low";
+	case GL_DEBUG_SEVERITY_NOTIFICATION:	return "notification";
+	default:								exit(EXIT_FAILURE);
+	}
+}
+
+static void error(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
+	const GLchar* message, const void* userParam)
+{
+	std::cerr << "GL ERROR:" << std::endl;
+	std::cerr << "  source:     " << errorSource(source) << std::endl;
+	std::cerr << "  type:       " << errorType(type) << std::endl;
+	std::cerr << "  severity:   " << errorSeverity(severity) << std::endl;
+	std::cerr << "  debug call: " << std::endl << message << std::endl << std::endl;
+	std::cerr << "Press <return>.";
+	std::cin.ignore();
+}
+
+void setupErrorCallback()
+{
+	glEnable(GL_DEBUG_OUTPUT);
+	glDebugMessageCallback(error, 0);
+	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, 0, GL_TRUE);
+	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, 0, GL_FALSE);
+	glDebugMessageControl(GL_DEBUG_SOURCE_API, GL_DEBUG_TYPE_PERFORMANCE, GL_DEBUG_SEVERITY_MEDIUM, 0, 0, GL_FALSE);
+	// params: source, type, severity, count, ids, enabled
+}
+
+///////////////////////////////////////////////// ERROR HANDLING (All versions)
+
+static const std::string errorString(GLenum error)
+{
+	switch (error) {
+	case GL_NO_ERROR:
+		return "No error has been recorded.";
+	case GL_INVALID_ENUM:
+		return "An unacceptable value is specified for an enumerated argument.";
+	case GL_INVALID_VALUE:
+		return "A numeric argument is out of range.";
+	case GL_INVALID_OPERATION:
+		return "The specified operation is not allowed in the current state.";
+	case GL_INVALID_FRAMEBUFFER_OPERATION:
+		return "The framebuffer object is not complete.";
+	case GL_OUT_OF_MEMORY:
+		return "There is not enough memory left to execute the command.";
+	case GL_STACK_UNDERFLOW:
+		return "An attempt has been made to perform an operation that would cause an internal stack to underflow.";
+	case GL_STACK_OVERFLOW:
+		return "An attempt has been made to perform an operation that would cause an internal stack to overflow.";
+	default: exit(EXIT_FAILURE);
+	}
+}
+
+static bool isOpenGLError()
+{
+	bool isError = false;
+	GLenum errCode;
+	while ((errCode = glGetError()) != GL_NO_ERROR) {
+		isError = true;
+		std::cerr << "OpenGL ERROR [" << errorString(errCode) << "]." << std::endl;
+	}
+	return isError;
+}
+
+static void checkOpenGLError(std::string error)
+{
+	if (isOpenGLError()) {
+		std::cerr << error << std::endl;
+		exit(EXIT_FAILURE);
+	}
+}
+
--- AVT_start/AVT_start/Engine/SourceFiles/main.cpp
+++ AVT_start/AVT_start/Engine/SourceFiles/main.cpp
@@ -56,115 +56,6 @@ Vector3D coords[] = {
 		Vector3D(0.6666, -1, 1),
 };
 
-////////////////////////////////////////////////// ERROR CALLBACK (OpenGL 4.3+)
-
-static const std::string errorSource(GLenum source)
-{
-	switch (source) {
-	case GL_DEBUG_SOURCE_API:				return "API";
-	case GL_DEBUG_SOURCE_WINDOW_SYSTEM:		return "window system";
-	case GL_DEBUG_SOURCE_SHADER_COMPILER:	return "shader compiler";
-	case GL_DEBUG_SOURCE_THIRD_PARTY:		return "third party";
-	case GL_DEBUG_SOURCE_APPLICATION:		return "application";
-	case GL_DEBUG_SOURCE_OTHER:				return "other";
-	default:								exit(EXIT_FAILURE);
-	}
-}
-
-static const std::string errorType(GLenum type)
-{
-	switch (type) {
-	case GL_DEBUG_TYPE_ERROR:				return "error";
-	case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:	return "deprecated behavior";
-	case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:	return "undefined behavior";
-	case GL_DEBUG_TYPE_PORTABILITY:			return "portability issue";
-	case GL_DEBUG_TYPE_PERFORMANCE:			return "performance issue";
-	case GL_DEBUG_TYPE_MARKER:				return "stream annotation";
-	case GL_DEBUG_TYPE_PUSH_GROUP:			return "push group";
-	case GL_DEBUG_TYPE_POP_GROUP:			return "pop group";
-	case GL_DEBUG_TYPE_OTHER_ARB:			return "other";
-	default:								exit(EXIT_FAILURE);
-	}
-}
-
-static const std::string errorSeverity(GLenum severity)
-{
-	switch (severity) {
-	case GL_DEBUG_SEVERITY_HIGH:			return "high";
-	case GL_DEBUG_SEVERITY_MEDIUM:			return "medium";
-	case GL_DEBUG_SEVERITY_LOW:				return "low";
-	case GL_DEBUG_SEVERITY_NOTIFICATION:	return "notification";
-	default:								exit(EXIT_FAILURE);
-	}
-}
-
-static void error(GLenum source, GLenum type, GLuint id, GLenum severity, GLsizei length,
-	const GLchar* message, const void* userParam)
-{
-	std::cerr << "GL ERROR:" << std::endl;
-	std::cerr << "  source:     " << errorSource(source) << std::endl;
-	std::cerr << "  type:       " << errorType(type) << std::endl;
-	std::cerr << "  severity:   " << errorSeverity(severity) << std::endl;
-	std::cerr << "  debug call: " << std::endl << message << std::endl << std::endl;
-	std::cerr << "Press <return>.";
-	std::cin.ignore();
-}
-
-void setupErrorCallback()
-{
-	glEnable(GL_DEBUG_OUTPUT);
-	glDebugMessageCallback(error, 0);
-	glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, 0, GL_TRUE);
-	glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DEBUG_SEVERITY_NOTIFICATION, 0, 0, GL_FALSE);
-	glDebugMessageControl(GL_DEBUG_SOURCE_API, GL_DEBUG_TYPE_PERFORMANCE, GL_DEBUG_SEVERITY_MEDIUM, 0, 0, GL_FALSE);
-	// params: source, type, severity, count, ids, enabled
-}
-
-///////////////////////////////////////////////// ERROR HANDLING (All versions)
-
-static const std::string errorString(GLenum error)
-{
-	switch(error) {
-	case GL_NO_ERROR:
-		return "No error has been recorded.";
-	case GL_INVALID_ENUM:
-		return "An unacceptable value is specified for an enumerated argument.";
-	case GL_INVALID_VALUE:
-		return "A numeric argument is out of range.";
-	case GL_INVALID_OPERATION:
-		return "The specified operation is not allowed in the current state.";
-	case GL_INVALID_FRAMEBUFFER_OPERATION:
-		return "The framebuffer object is not complete.";
-	case GL_OUT_OF_MEMORY:
-		return "There is not enough memory left to execute the command.";
-	case GL_STACK_UNDERFLOW:
-		return "An attempt has been made to perform an operation that would cause an internal stack to underflow.";
-	case GL_STACK_OVERFLOW:
-		return "An attempt has been made to perform an operation that would cause an internal stack to overflow.";
-	default: exit(EXIT_FAILURE);
-	}
-}
-
-static bool isOpenGLError() 
-{
-	bool isError = false;
-	GLenum errCode;
-	while ((errCode = glGetError()) != GL_NO_ERROR) {
-		isError = true;
-		std::cerr << "OpenGL ERROR [" << errorString(errCode) << "]." << std::endl;
-	}
-	return isError;
-}
-
-static void checkOpenGLError(std::string error)
-{
-	if (isOpenGLError()) {
-		std::cerr << error << std::endl;
-		exit(EXIT_FAILURE);
-	}
-}
-
 /////////////////////////////////////////////////////////////////////// SHADERs
 
 struct ShaderSource {
@@ -433,7 +324,7 @@ void glfw_error_callback(int error, const char* description)
 	std::cerr << "GLFW Error: " << description << std::endl;
 }
 
-GLFWwindow* setupWindow(int winx, int winy, const char* title, 
+GLFWwindow* setupWindow(int winx, int winy, const char* title,
 	int is_fullscreen, int is_vsync)
 {
 	GLFWmonitor* monitor = is_fullscreen ? glfwGetPrimaryMonitor() : 0;
@@ -461,7 +352,7 @@ void setupCallbacks(GLFWwindow* win)
 	glfwSetWindowSizeCallback(win, window_size_callback);
 }
 
-GLFWwindow* setupGLFW(int gl_major, int gl_minor, 
+GLFWwindow* setupGLFW(int gl_major, int gl_minor,
 	int winx, int winy, const char* title, int is_fullscreen, int is_vsync)
 {
 	glfwSetErrorCallback(glfw_error_callback);
@@ -488,7 +379,7 @@ void setupGLEW()
 	// Allow extension entry points to be loaded even if the extension isn't 
 	// present in the driver's extensions string.
 	GLenum result = glewInit();
-	if (result != GLEW_OK) 
+	if (result != GLEW_OK)
 	{
 		std::cerr << "ERROR glewInit: " << glewGetString(result) << std::endl;
 		exit(EXIT_FAILURE);
@@ -524,6 +415,7 @@ void setupOpenGL(int winx, int winy)
 	glFrontFace(GL_CCW);
 	glViewport(0, 0, winx, winy);
 }
+
 
 GLFWwindow* setup(int major, int minor, 
 	int winx, int winy, const char* title, int is_fullscreen, int is_vsync)
